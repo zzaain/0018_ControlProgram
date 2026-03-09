769751749edc0dbe67bbd9655a1d90fd395c5bb2
zzaain <sheriellaghzain@gmail.com>
2026-03-09 11:19:53 +0700

    menambahkan input nilai bahasa inggris dan matematika

--- Nesteedif1.cpp
+++ Nesteedif1.cpp
@@ -19,4 +19,9 @@ int main()
     float nilBI, nilMT, rerata;
     string status;
 
-    
+    cout << "Masukkan nilai Bahasa Inggris = ";
+    cin >> nilBI;
+    cout << "Masukkan nilai Matematika = ";
+    cin >> nilMT;
+
+   
